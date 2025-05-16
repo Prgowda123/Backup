a94914174bc2ac1b915eb6a2987d0dec7e2a8a05
Prgowda123 <pallurgowda13@gmail.com>
2025-05-16 09:02:02 +0530

    ddt

--- Batchentry.java
+++ Batchentry.java
@@ -1,39 +1,46 @@
-package apc_kk;
+package ddt;
 
 import org.json.simple.JSONObject;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.http.Headers;
-import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 public class Batchentry {
-@Test
-public void reg()
-{
-	RestAssured.baseURI="http://172.10.1.159:9025";
-	RequestSpecification req = RestAssured.given();
-	JSONObject obj = new JSONObject();
-	obj.put("userCode", "");
-	obj.put("name", "poiu");
-	obj.put("phoneNo", "9874565135");
-	obj.put("email", "pomm54@gmail.com");
-	obj.put("userName", "onnegowda");
-	obj.put("password", "07-11-2002");
-	obj.put("isAdmin", true);
-	req.contentType(ContentType.JSON);
-	req.body(obj.toJSONString());
-	
-	
-	Response res = req.request(Method.POST,"/api/Account/register-batchentrymember");
-	Headers ser = res.getHeaders();
-	System.out.println(res.statusCode());
-	System.out.println(ser);
-	System.out.println(res.asPrettyString());
-	
-}
-}
 
+    @DataProvider(name = "userDataFromExcel")
+    public Object[][] getDataFromExcel() throws Exception {
+        return ExcelReader.getExcelData("D://Automation_data//api_ddt.xlsx", "Sheet1"); // Update the path and sheet name
+    }
+
+    @Test(dataProvider = "userDataFromExcel")
+    public void reg(String userCode, String name, String phoneNo, String email, String userName, String password, String isAdmin) {
+        RestAssured.baseURI = "http://172.10.1.159:9025";
+        RequestSpecification req = RestAssured.given();
+
+        // Create JSON object for the request body
+        JSONObject obj = new JSONObject();
+        obj.put("userCode", userCode);
+        obj.put("name", name);
+        obj.put("phoneNo", phoneNo);
+        obj.put("email", email);
+        obj.put("userName", userName);
+        obj.put("password", password);
+        obj.put("isAdmin", Boolean.parseBoolean(isAdmin)); // Convert string "true"/"false" to boolean
+
+        req.contentType(ContentType.JSON);
+        req.body(obj.toJSONString());
+
+        // Send POST request
+        Response res = req.post("/api/Account/register-batchentrymember");
+
+        // Get the response and headers
+        Headers ser = res.getHeaders();
+        System.out.println(res.statusCode());
+        System.out.println(ser);
+        System.out.println(res.asPrettyString());
+    }
+}
